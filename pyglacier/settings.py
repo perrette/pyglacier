fcf4c8c274e272735aebe3796169e400c73c8d1b
perrette <mahe.perrette@pik-potsdam.de>
2015-04-09 17:12:06 +0200

    set_code_dir func in settings

--- pyglacier/settings.py
+++ pyglacier/settings.py
@@ -4,9 +4,14 @@ import os
 
 # CODEDIR = os.path.dirname(__file__)+'/../'
 # Assume the program is started from the code directory, unless otherwise indicated
-CODEDIR = './'
-GLACIERLIB = CODEDIR +'.obj/wrapper.so'
-NML = CODEDIR +'params.nml'
-OUTDIR = './out/'  # out directory in calling directory
-INFILE = "./input/glacier.nc"
-RSTFILE = ""  # not restart file by default
+
+def set_codedir(codedir):
+    global CODEDIR, GLACIERLIB, NML, OUTDIR, INFILE, RSTFILE
+    CODEDIR = codedir + '/'
+    GLACIERLIB = CODEDIR +'.obj/wrapper.so'
+    NML = CODEDIR +'params.nml'
+    OUTDIR = './out/'  # out directory in calling directory
+    INFILE = "./input/glacier.nc"
+    RSTFILE = ""  # not restart file by default
+
+set_codedir('./')
